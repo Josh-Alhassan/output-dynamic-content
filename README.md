92ad42e0067793e78919f5a9ce3183e578daa80d
Saint-Josh <saintjosh26@gmail.com>
2022-05-07 17:55:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fdtehw
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fdtehw)
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  const expenseDate = new Date(2022, 5, 7);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67
+
+  return (
+    <div className = "expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className = "expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className = "expense-item__price">£ {expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
