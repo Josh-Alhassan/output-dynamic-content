39398f62021bf5635cd76e7ca3064027944501b9
Saint-Josh <saintjosh26@gmail.com>
2022-05-09 15:11:18 +0100

    split components into multiple component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import './ExpenseItem.css';
 
+import ExpenseDate from './ExpenseDate';
+
 function ExpenseItem(props) {
   // const expenseDate = new Date(2022, 5, 7);
   // const expenseTitle = 'Car Insurance';
   // const expenseAmount = 294.67
 
-  const month = props.date.toLocaleString("en-Us", {month: "long"});
-  const day = props.date.toLocaleString("en-Us", {day: "2-digit"});
-  const year = props.date.getFullYear();
+  // const month = props.date.toLocaleString("en-Us", {month: "long"});
+  // const day = props.date.toLocaleString("en-Us", {day: "2-digit"});
+  // const year = props.date.getFullYear();
 
   return (
-    <div className = "expense-item">
-      <div>
+    <div className="expense-item">
+      {/* <div>
         <div>{month} </div>
         <div>{year}</div>
         <div>{day}</div>
-      </div>
+      </div> */}
+
+      <ExpenseDate date={props.date} />
       <div>{props.date.toISOString()}</div>
-      <div className = "expense-item__description">
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className = "expense-item__price">£ {props.amount}</div>
+        <div className="expense-item__price">£ {props.amount}</div>
       </div>
     </div>
   );
