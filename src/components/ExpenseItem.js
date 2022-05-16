1cb20c9232c802c63cce42f7f7e0a505af5f508f
Saint-Josh <saintjosh26@gmail.com>
2022-05-16 09:37:17 +0100

    working with states

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './ExpenseItem.css';
 import Card from './Card';
+import React, { useState } from 'react';
 
 import ExpenseDate from './ExpenseDate';
 
 const ExpenseItem = (props) => {
   // Button Event Handler function {}
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
+    setTitle('updated!')
     console.log('Button Clicked')
   }
   // const expenseDate = new Date(2022, 5, 7);
@@ -28,7 +31,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date={props.date} />
       {/* <div>{props.date.toISOString()}</div> */}
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">£ {props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
